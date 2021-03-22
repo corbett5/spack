d461baf13bf020532e03fccd50c1796d62b17fec
Benjamin Curtice Corbett <corbett5@llnl.gov>
2021-03-22 16:58:50 -0700

    Squash

--- var/spack/repos/builtin/packages/lvarray/package.py
+++ var/spack/repos/builtin/packages/lvarray/package.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 # Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
-=======
-# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
->>>>>>> Added LvArray package.
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
