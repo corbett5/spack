10cbf382f7c93c19fbe8bb69bab26b939adde0fc
Benjamin Curtice Corbett <corbett5@llnl.gov>
2021-05-25 09:25:48 -0700

    Updated hypre to add CUDA support.

--- var/spack/repos/builtin/packages/hypre/package.py
+++ var/spack/repos/builtin/packages/hypre/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -8,7 +8,7 @@ import os
 import sys
 
 
-class Hypre(Package):
+class Hypre(Package, CudaPackage):
     """Hypre is a library of high performance preconditioners that
        features parallel multigrid methods for both structured and
        unstructured grid problems."""
@@ -19,8 +19,10 @@ class Hypre(Package):
 
     maintainers = ['ulrikeyang', 'osborn9', 'balay']
 
+    test_requires_compiler = True
+
     version('develop', branch='master')
-    version('2.21.0', commit='2186a8fb343f84f109405eed3e298cb1a2615e95') # Note this is not the official release.
+    version('2.20.1', commit='2186a8fb343f84f109405eed3e298cb1a2615e95') # Note this is not the official release.
     version('2.20.0', sha256='5be77b28ddf945c92cde4b52a272d16fb5e9a7dc05e714fc5765948cba802c01')
     version('2.19.0', sha256='466b19d8a86c69989a237f6f03f20d35c0c63a818776d2cd071b0a084cffeba5')
     version('2.18.2', sha256='28007b5b584eaf9397f933032d8367788707a2d356d78e47b99e551ab10cc76a')
@@ -59,6 +61,8 @@ class Hypre(Package):
     variant('openmp', default=False, description='Enable OpenMP support')
     variant('debug', default=False,
             description='Build debug instead of optimized version')
+    variant('unified-memory', default=False, description='Use unified memory')
+    variant('cusparse', default=False, description='Use cusparse')
 
     # Patch to add ppc64le in config.guess
     patch('ibm-ppc64le.patch', when='@:2.11.1')
@@ -69,11 +73,18 @@ class Hypre(Package):
     patch('superlu-dist-link-2.15.0.patch', when='+superlu-dist @2.15:2.16.0')
     patch('superlu-dist-link-2.14.0.patch', when='+superlu-dist @:2.14.0')
     patch('hypre21800-compat.patch', when='@2.18.0')
+    # Patch to get config flags right
+    patch('detect-compiler.patch', when='@2.15.0:2.20.0')
 
     depends_on("mpi", when='+mpi')
     depends_on("blas")
     depends_on("lapack")
     depends_on('superlu-dist', when='+superlu-dist+mpi')
+
+    conflicts('+cuda', when='+int64')
+    conflicts('cuda_arch=none', when='+cuda')
+    conflicts('+unified-memory', when='~cuda')
+    conflicts('+cusparse', when='~cuda')
 
     # Patch to build shared libraries on Darwin does not apply to
     # versions before 2.13.0
@@ -97,44 +108,32 @@ class Hypre(Package):
 
         return url.format(version)
 
-    def install(self, spec, prefix):
+    def _configure_args(self):
+        spec = self.spec
         # Note: --with-(lapack|blas)_libs= needs space separated list of names
-        lapack_libs = spec['lapack'].libs
-        blas_libs = spec['blas'].libs
+        lapack = spec['lapack'].libs
+        blas = spec['blas'].libs
 
         configure_args = [
             '--prefix=%s' % prefix,
-            '--with-lapack-libs=%s' % ' '.join(lapack_libs.names),
-            '--with-lapack-lib-dirs=%s' % ' '.join(lapack_libs.directories),
-            '--with-blas-libs=%s' % ' '.join(blas_libs.names),
-            '--with-blas-lib-dirs=%s' % ' '.join(blas_libs.directories)
+            '--with-lapack-libs=%s' % ' '.join(lapack.names),
+            '--with-lapack-lib-dirs=%s' % ' '.join(lapack.directories),
+            '--with-blas-libs=%s' % ' '.join(blas.names),
+            '--with-blas-lib-dirs=%s' % ' '.join(blas.directories)
         ]
 
         if '+mpi' in self.spec:
-            configure_args.append('CC=%s' % spec['mpi'].mpicc)
-            configure_args.append('CXX=%s' % spec['mpi'].mpicxx)
-            configure_args.append('F77=%s' % spec['mpi'].mpif77)
+            os.environ['CC'] = spec['mpi'].mpicc
+            os.environ['CXX'] = spec['mpi'].mpicxx
+            os.environ['F77'] = spec['mpi'].mpif77
             configure_args.append('--with-MPI')
         else:
-            configure_args.append('CC=%s' % spack_cc)
-            configure_args.append('CXX=%s' % spack_cxx)
-            configure_args.append('F77=%s' % spack_f77)
             configure_args.append('--without-MPI')
-
-        cflags = ' '.join(spec.compiler_flags['cflags'])
-        cxxflags = ' '.join(spec.compiler_flags['cxxflags'])
-        fflags = ' '.join(spec.compiler_flags['fflags'])
 
         if '+openmp' in self.spec:
             configure_args.append('--with-openmp')
-            cflags += ' ' + self.compiler.openmp_flag
-            configure_args.append('LDFLAGS=%s' % self.compiler.openmp_flag)
         else:
             configure_args.append('--without-openmp')
-            
-        configure_args.append('CFLAGS=%s' % cflags)
-        configure_args.append('CXXFLAGS=%s' % cxxflags)
-        configure_args.append('FCFLAGS=%s' % fflags)
 
         if '+int64' in self.spec:
             configure_args.append('--enable-bigint')
@@ -172,6 +171,46 @@ class Hypre(Package):
         else:
             configure_args.append("--disable-debug")
 
+        if '+cuda' in self.spec:
+            configure_args.extend([
+                '--with-cuda',
+                '--enable-curand',
+                '--enable-cub'
+            ])
+        else:
+            configure_args.extend([
+                '--without-cuda',
+                '--disable-curand',
+                '--disable-cub'
+            ])
+
+        if '+unified-memory' in self.spec:
+            configure_args.append('--enable-unified-memory')
+
+        if '+cusparse' in self.spec:
+            configure_args.append('--enable-cusparse')
+
+        return configure_args
+
+    def setup_build_environment(self, env):
+        if '+mpi' in self.spec:
+            env.set('CC', self.spec['mpi'].mpicc)
+            env.set('CXX', self.spec['mpi'].mpicxx)
+            env.set('F77', self.spec['mpi'].mpif77)
+
+        if '+cuda' in self.spec:
+            env.set('CUDA_HOME', self.spec['cuda'].prefix)
+            env.set('CUDA_PATH', self.spec['cuda'].prefix)
+            cuda_arch = self.spec.variants['cuda_arch'].value
+            if cuda_arch:
+                arch_sorted = list(sorted(cuda_arch, reverse=True))
+                env.set('HYPRE_CUDA_SM', arch_sorted[0])
+            # In CUDA builds hypre currently doesn't handle flags correctly
+            env.append_flags(
+                'CXXFLAGS', '-O2' if '~debug' in self.spec else '-g')
+
+    def install(self, spec, prefix):
+        configure_args = self._configure_args()
         # Hypre's source is staged under ./src so we'll have to manually
         # cd into it.
         with working_dir("src"):
@@ -187,6 +226,27 @@ class Hypre(Package):
                 sstruct('-in', 'test/sstruct.in.default', '-solver', '40',
                         '-rhsone')
             make("install")
+
+    # @run_after('install')
+    # def cache_test_sources(self):
+    #     srcs = ['src/examples']
+    #     self.cache_extra_test_sources(srcs)
+
+    def test(self):
+        """Perform smoke test on installed HYPRE package."""
+
+        if '+mpi' in self.spec:
+            examples_dir = join_path(self.install_test_root, 'src/examples')
+            with working_dir(examples_dir, create=False):
+                make("HYPRE_DIR=" + self.prefix, "bigint")
+
+                reason = "test: ensuring HYPRE examples run"
+                self.run_test('./ex5big', [], [], installed=True,
+                              purpose=reason, skip_missing=True, work_dir='.')
+                self.run_test('./ex15big', [], [], installed=True,
+                              purpose=reason, skip_missing=True, work_dir='.')
+
+                make("distclean")
 
     @property
     def headers(self):
